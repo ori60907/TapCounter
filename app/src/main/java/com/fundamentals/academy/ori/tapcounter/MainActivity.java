91b1e9cd3b03ec831a04a31e92f2677f0c5b850f
Ori home computer <ori60907@gmail.com>
2017-11-17 15:47:20 +0200

    created the tap counter

--- app/src/main/java/com/fundamentals/academy/ori/tapcounter/MainActivity.java
+++ app/src/main/java/com/fundamentals/academy/ori/tapcounter/MainActivity.java
@@ -2,12 +2,23 @@ package com.fundamentals.academy.ori.tapcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    private TextView counter_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter_tv = findViewById(R.id.tv_counter);
+    }
+
+    public void increaseNumber(View view) {
+        counter++;
+        counter_tv.setText(String.valueOf(counter));
     }
 }
